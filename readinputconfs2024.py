498257e836fd3dfdf47e1996e81517e091767af1
Stefan Karlsson <github@k-son.se>
2026-01-03 18:12:35 +0100

    Njae

--- readinputconfs2024.py
+++ readinputconfs2024.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 import os
 
 path = r'C:\Users\stefa\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\SystemAppData\wgs\000901F27997511D_00000000000000000000000069F80140'
@@ -11,12 +11,45 @@ with os.scandir(path) as d:
             with os.scandir(fr'{path}\{e.name}') as dd:
                 for f in dd:
                     print(f.path)
-                    if not f.name.endswith('.1'):
-                        tree = ET.parse(f.path)
-                        root = tree.getroot()
-                        print(root.tag)
-"""                         for child in root:
-                            print(child.tag, child.attrib)  """
+                    """
+                    if not f.name[:4]=='cont':
+                        with open(f.path, 'r', errors = 'ignore') as fh:
+                            data = fh.read()
+                            #print(data)
+                        try:
+                            Bs_data = BeautifulSoup(data, 'xml')
+                            b_unique = Bs_data.find_all('FriendlyName')
+                            print(b_unique)
+                        except:
+                            print('Skit')
+                    """
+                    import xml.etree.ElementTree as ET
+                    tree = ET.parse(f.path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                         
 
 
---
+++ sandbox.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+
+with open('dict.xml', 'r') as f:
+    data = f.read()
+
+    
